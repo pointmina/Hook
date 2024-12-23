82fb2cd3e7d77db3712d98f13185dad5a161c4ac
hanto <pointmina@gmail.com>
2024-12-24 00:39:36 +0900

    빌드 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
 
